8be048d9ea3c48da38219afa1381e25c0c435b86
Aisha Osman <aisha.osman09@gmail.com>
2021-12-12 17:28:59 -0600

    develop

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -7,6 +7,16 @@ class Tag extends Model {}
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNUll: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      
+    }
   },
   {
     sequelize,
